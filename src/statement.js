9197b98919c0b3aaf80656ec70aa9ce55faa81e3
vinod <vinod@wealthos.cloud>
2026-01-17 15:34:00 +0530

    use customer from data instead of invoice

--- src/statement.js
+++ src/statement.js
@@ -1,11 +1,12 @@
 
 function statement (invoice, plays) {
     const statementData = {}
+    statementData.customer = invoice.customer;
     return renderPlainText(statementData, invoice, plays);
 }
 
 function renderPlainText(data, invoice, plays) {
-    let result = `Statement for ${invoice.customer}\n`;
+    let result = `Statement for ${data.customer}\n`;
     for (let perf of invoice.performances) {
         result += ` ${getPlay(perf).name}: ${(usd(amountForPlay(perf)))} (${perf.audience} seats)\n`;
     }
