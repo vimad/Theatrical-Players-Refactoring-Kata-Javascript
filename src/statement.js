730b214feba974b11d3c2723157ec60ac4ccf897
vinod <vinod@wealthos.cloud>
2026-01-17 15:36:23 +0530

    remove invoice from renderPlainText function

--- src/statement.js
+++ src/statement.js
@@ -3,10 +3,10 @@ function statement (invoice, plays) {
     const statementData = {}
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances;
-    return renderPlainText(statementData, invoice, plays);
+    return renderPlainText(statementData, plays);
 }
 
-function renderPlainText(data, invoice, plays) {
+function renderPlainText(data, plays) {
     let result = `Statement for ${data.customer}\n`;
     for (let perf of data.performances) {
         result += ` ${getPlay(perf).name}: ${(usd(amountForPlay(perf)))} (${perf.audience} seats)\n`;
