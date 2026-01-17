2e6ec9d8d5ad3cfb3580e8a36efa5cdb96279eac
vinod <vinod@wealthos.cloud>
2026-01-17 15:55:23 +0530

    use amount from perf object

--- src/statement.js
+++ src/statement.js
@@ -42,7 +42,7 @@ function statement (invoice, plays) {
 function renderPlainText(data) {
     let result = `Statement for ${data.customer}\n`;
     for (let perf of data.performances) {
-        result += ` ${perf.play.name}: ${(usd(amountFor(perf)))} (${perf.audience} seats)\n`;
+        result += ` ${perf.play.name}: ${(usd(perf.amount))} (${perf.audience} seats)\n`;
     }
     result += `Amount owed is ${format(totalAmount() / 100)}\n`;
     result += `You earned ${(totalVolumeCredits())} credits\n`;
@@ -55,29 +55,6 @@ function renderPlainText(data) {
                 minimumFractionDigits: 2
             }).format(number);
     }
-
-    function amountFor(perf) {
-        let thisAmount = 0;
-        switch (perf.play.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${perf.play.type}`);
-        }
-        return thisAmount;
-    }
-
     function volumeCreditFor(perf) {
         let volumeCredits = Math.max(perf.audience - 30, 0);
         if ("comedy" === perf.play.type) volumeCredits += Math.floor(perf.audience / 5);
@@ -99,7 +76,7 @@ function renderPlainText(data) {
     function totalAmount() {
         let totalAmount = 0;
         for (let perf of data.performances) {
-            totalAmount += amountFor(perf);
+            totalAmount += perf.amount;
         }
         return totalAmount;
     }
