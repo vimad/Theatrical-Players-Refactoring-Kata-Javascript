3704ca7fdb6ca62f1f6aa9c6208841b996ff769d
vinod <vinod@wealthos.cloud>
2026-01-17 15:42:34 +0530

    remove plays parameter from renderPlainText function

--- src/statement.js
+++ src/statement.js
@@ -3,7 +3,7 @@ function statement (invoice, plays) {
     const statementData = {}
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformance);
-    return renderPlainText(statementData, plays);
+    return renderPlainText(statementData);
 
     function enrichPerformance(performance) {
         const result = Object.assign({}, performance);
@@ -16,7 +16,7 @@ function statement (invoice, plays) {
     }
 }
 
-function renderPlainText(data, plays) {
+function renderPlainText(data) {
     let result = `Statement for ${data.customer}\n`;
     for (let perf of data.performances) {
         result += ` ${perf.play.name}: ${(usd(amountForPlay(perf)))} (${perf.audience} seats)\n`;
